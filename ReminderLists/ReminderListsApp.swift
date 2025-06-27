7610f23330d19d23eb443f13f2ede51acc90ce17
liamdonlan <liamjdonlan@gmail.com>
2025-06-26 20:19:30 -0400

    Initial Commit

---
+++ ReminderLists/ReminderListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderListsApp.swift
+//  ReminderLists
+//
+//  Created by Liam Donlan on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ReminderListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReminderListsTests/ReminderListsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderListsTests.swift
+//  ReminderListsTests
+//
+//  Created by Liam Donlan on 6/26/25.
+//
+
+import Testing
+@testable import ReminderLists
+
+struct ReminderListsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
